d19645f8d3eb9399606b32401aad421ce21599dd
yvesauad <yvesauad@gmail.com>
2022-04-29 08:20:26 +0200

    v0.0.17: proper way of installing package_data in setup

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup
-from glob import glob
-
-correction_files = glob('nionswift_plugin/orsay_suite/aux_files/config/Gain_Merlin/*/*')
 
 setup(
     name="OrsaySuite",
-    version="0.0.16",
+    version="0.0.17",
     author="Yves Auad",
     description="Tools for analyzing data using Hyperspy",
     url="https://github.com/yvesauad/yvorsay-instrument",
     packages=['nionswift_plugin.orsay_suite'],
+    packages_dir={'nionswift_plugin.orsay_suite': 'src/nionswift_plugin/orsay_suite'},
+    package_data={'nionswift_plugin.orsay_suite': [
+        'aux_files/config/Gain_Merlin/60kV-06-09-2021/*',
+        'aux_files/config/Gain_Merlin/100kV-12-07-2021/*',
+        'aux_files/config/Gain_Merlin/200kV-29-03-2022/*'
+    ]},
     python_requires='>=3.8.5',
     include_package_data=True,
-    data_files=[('', correction_files)],
 )
