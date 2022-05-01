9bfe7a1ca5392547c23b5bb5dd4ffbb41ea3b807
Luiz Tizei <luiz.galvao-tizei@universite-paris-saclay.fr>
2022-05-01 09:11:40 +0200

    Working local loader in linux

--- nionswift_plugin/aux_files/config/read_data_gain.py
+++ nionswift_plugin/aux_files/config/read_data_gain.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import logging
 import hyperspy.api as hs
 class FileManager:
@@ -8,17 +9,19 @@ class FileManager:
         self.filename = str(ht)+'kV_Thresh_'+str(threshold)+'.hspy'
 
         if (os.name == 'posix'): #this is linux
-            abs_path = os.path.abspath('/home/ProgramData/Microscope/' + self.filename)
+            abs_path = os.path.abspath(str(Path.home())+'/ProgramData/Microscope/Gain_Merlin/'+str(ht)+'kV/' + self.filename)
             try:
                 with open(abs_path) as savfile:
                     self.abs_path = abs_path
-                    self.gain = hs.load(abs_path + self.filename)
+                    self.gain = hs.load(abs_path)
+                    print('read local gain')
             except FileNotFoundError:
                 abs_path = os.path.join(os.path.dirname(__file__)+'/Gain_Merlin/' +str(ht)+'kV', self.filename)  # this works
                 self.abs_path = abs_path
                 with open(abs_path) as savfile:
                     self.abs_path = abs_path
                     self.gain = hs.load(abs_path)
+                    print('read global gain')
 
         if (os.name == 'nt'):  # this is Windows
             print('Read_data_gain : Not implemented for Windows')
