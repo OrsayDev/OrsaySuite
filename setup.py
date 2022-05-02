dd9327ac8f0d63ae9245e3c2ac84b8b258086864
yvesauad <yvesauad@gmail.com>
2022-05-02 13:13:27 +0200

    v0.0.21: minor setup change

--- setup.py
+++ setup.py
@@ -2,16 +2,14 @@ from setuptools import setup
 
 setup(
     name="OrsaySuite",
-    version="0.0.20",
+    version="0.0.21",
     author="Yves Auad",
     description="Tools for analyzing data using Hyperspy",
     url="https://github.com/yvesauad/yvorsay-instrument",
     packages=['nionswift_plugin.orsay_suite'],
     packages_dir={'nionswift_plugin.orsay_suite': 'src/nionswift_plugin/orsay_suite'},
     package_data={'nionswift_plugin.orsay_suite': [
-        'aux_files/config/Gain_Merlin/60kV-06-09-2021/*',
-        'aux_files/config/Gain_Merlin/100kV-12-07-2021/*',
-        'aux_files/config/Gain_Merlin/200kV-29-03-2022/*'
+        'aux_files/config/Gain_Merlin/*',
     ]},
     python_requires='>=3.8.5',
     include_package_data=True,
